11c947630ee78609851735393277602d3dd75927
Furqan220 <dfantasy637@gmail.com>
2023-07-23 05:53:24 -0700

    chat initiated

--- index.js
+++ index.js
@@ -12,14 +12,23 @@ var io = require("socket.io")(server)
 
 
 app.use(express.json());
+var clients = {};
 
 io.on("connection",(socket)=>{
     console.log("Connected");
     console.log(socket.id,"has joined");
-    socket.on("/test",(msg)=>{
-        console.log(msg)
+    socket.on("signIn",(id)=>{
+        console.log(id)
+        clients[id] = socket;
+        console.log(clients);
     });
-
+    socket.on("message",(msg)=>{
+        console.log(clients);
+        let targetId = msg["targetId"];
+        console.log(msg);
+        if(clients[targetId])clients[targetId].emit("message",msg)
+    
+    });
 });
 
 server.listen(port ,"0.0.0.0",()=>{
